aaadae3103cd9bf65036c483775f853009ea9ded
tiarapr <tiaraputriramadhani1@gmail.com>
2025-06-10 21:09:07 +0700

    remove auth header key

--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,5 @@ module.exports = {
   HOST: process.env.HOST,
   BASIC_AUTH_USERNAME: process.env.BASIC_AUTH_USERNAME,
   BASIC_AUTH_PASSWORD: process.env.BASIC_AUTH_PASSWORD,
-  AUTH_HEADER_KEY: process.env.AUTH_HEADER_KEY,
   ACCESS_TOKEN_KEY: process.env.ACCESS_TOKEN_KEY,
 };
